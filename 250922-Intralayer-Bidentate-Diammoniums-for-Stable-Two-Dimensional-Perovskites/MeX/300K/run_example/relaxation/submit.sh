08e35dc2f6ab77fa5878e489f23d20ee310f7168
Zhichen Nian <znian@nd.edu>
2025-11-09 14:48:45 -0500

    Replace run1 by run_example

---
+++ 250922-Intralayer-Bidentate-Diammoniums-for-Stable-Two-Dimensional-Perovskites/MeX/300K/run_example/relaxation/submit.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+#SBATCH --job-name=300K_run7
+#SBATCH -N 1
+#SBATCH -n 64
+#SBATCH -t 4:00:00
+#SBATCH -A standby
+#SBATCH -o 300K_run7.out
+#SBATCH -e 300K_run7.err
+
+# Load default LAMMPS, compiler, and openmpi packages
+module load gcc/12.2.0 
+module load openmpi/4.1.4 
+module load lammps/20220623 
+
+# cd into the submission directory
+echo Running on host `hostname`
+echo Time is `date`
+t_start=$SECONDS
+
+# Submiting LAMMPS job for relaxation.in.init
+mpirun -np $SLURM_NTASKS lmp -in relaxation.in.init > LAMMPS_run.out &
+wait
+
+t_end=$SECONDS
+t_diff=$(( ${t_end} - ${t_start} ))
+eval "echo $(date -ud "@$t_diff" +'This job took $((%s/3600/24)) days %H hours %M minutes %S seconds to complete.')"
+echo Completion time is `date`
